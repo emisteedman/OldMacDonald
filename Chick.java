468e05008c49284248057176991c20d8503e617f
emisteedman <emsteedman@s.sfusd.edu>
2017-10-25 14:17:19 -0700

    changed oldmacdonald code

--- Chick.java
+++ Chick.java
@@ -1,4 +1,30 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal {     
+  private String myType;
+  private String mySound1;
+  private String mySound2;
+  private double probability = Math.random();
+  public Chick(String type, String sound1, String sound2){
+    myType = type;
+    mySound1 = sound1;
+    mySound2 = sound2;
+    
+  }
+  public Chick(){
+    myType = "unknown";
+    mySound1 = "unknown";
+    mySound2 = "unknown";
+  }
+  public String getSound(){
+    if (probability < 0.5){
+      return mySound1;
+    }
+    else
+      return mySound2;
+    }
+   
+  public String getType(){
+      return myType;
+  }
+    
+    
 }
--- Farm.java
+++ Farm.java
@@ -1,4 +1,14 @@
-class Farm 
-{     
-   //your code here
+class Farm {     
+  private Animal[] aBunchofAnimals = new Animal[3];
+  public Farm(){
+    aBunchofAnimals[0] = new NamedCow("Elsie","cow", "moo");
+    aBunchofAnimals[1] = new Chick("chick", "cheep", "cluck");
+    aBunchofAnimals[2] = new Pig("pig", "oink");
+  }
+  public void animalSounds(){
+    for (int nI = 0; nI < aBunchofAnimals.length; nI++){
+      System.out.println(aBunchofAnimals[nI].getType() + " goes " + aBunchofAnimals[nI].getSound());
+    }
+    System.out.println("The cow is known as " + (((NamedCow)aBunchofAnimals[0]).getName()));
+}
 }
